iustin/mt-st

commit 874d58d952d358efa85d24259cccc45191df57d6

Fix coverity scan warnings.
 * Fix the leaking `fd`.
 * Fix the possible overflow of strcat().

Signed-off-by: Gris Ge <fge@redhat.com>

stinit.c
@@ -333,8 +333,7 @@ find_pars(FILE *dbf, char *company, char *product, char *rev, devdef_tr *defs, i
                 continue;
             }
 
-            strcpy(modebuf, comptr);
-            strcat(modebuf, cp);
+            snprintf(defstr, sizeof(defstr)/sizeof(char), "%s%s", comptr, cp);
             *nextdef = c;
 
             if (verbose > 1)

mt.c
@@ -372,7 +372,7 @@ static void version()
 static void usage(int explain, int exit_code)
 {
     int ind;
-    char line[100];
+    int counter = 0;
 
     fprintf(stderr, "usage: mt [-v] [--version] [-h] [ -f device ] command [ "
                     "count ]\n");
@@ -380,17 +380,17 @@ static void usage(int explain, int exit_code)
     if (explain) {
         for (ind = 0; cmds[ind].cmd_name != NULL;) {
             if (ind == 0)
-                strcpy(line, "commands: ");
+                counter = fprintf(stderr, "commands: ");
             else
-                strcpy(line, "          ");
-            for (; cmds[ind].cmd_name != NULL; ind++) {
-                strcat(line, cmds[ind].cmd_name);
-                if (cmds[ind + 1].cmd_name != NULL)
-                    strcat(line, ", ");
+                counter = fprintf(stderr, "          ");
+            for ( ; cmds[ind].cmd_name != NULL; ind++) {
+                counter += fprintf(stderr, "%s", cmds[ind].cmd_name);
+                if (cmds[ind+1].cmd_name != NULL)
+                    counter += fprintf(stderr, ", ");
                 else
-                    strcat(line, ".");
-                if (strlen(line) >= 70 || cmds[ind + 1].cmd_name == NULL) {
-                    fprintf(stderr, "%s\n", line);
+                    counter += fprintf(stderr, ".");
+                if (counter >= 70 || cmds[ind+1].cmd_name == NULL) {
+                    fprintf(stderr, "\n");
                     ind++;
                     break;
                 }
@@ -716,6 +716,8 @@ static int do_show_options(int mtfd,
 
     if ((fd = open(fname, O_RDONLY)) < 0 || read(fd, buf, 20) < 0) {
         fprintf(stderr, "Can't read the sysfs file '%s'.\n", fname);
+	if (fd >= 0)
+		close(fd);
         return 2;
     }
     close(fd);

commit d5720ee85eb64a8e78254e289b2d8e224c705736

Fix stinit file parsing broken in 874d58d9
For some reason I missed this during the pull request (even if
trivial), thanks travis-ci for letting me know (indirectly :).

Fixes #14.

stinit.c
@@ -316,6 +316,7 @@ find_pars(FILE *dbf, char *company, char *product, char *rev, devdef_tr *defs, i
         SKIP_WHITE(comptr);
 
         for (; *nextdef != '\0';) {
+            int tmpsize;
             curdef = nextdef;
             SKIP_WHITE(curdef);
             for (nextdef++;
@@ -334,7 +335,14 @@ find_pars(FILE *dbf, char *company, char *product, char *rev, devdef_tr *defs, i
                 continue;
             }
 
-            snprintf(defstr, sizeof(defstr) / sizeof(char), "%s%s", comptr, cp);
+            if ((tmpsize = snprintf(modebuf, DEFMAX, "%s%s", comptr, cp)) >= DEFMAX) {
+                fprintf(stderr, "Definition too large for ('%s', '%s', '%s'): size %d, maximum is %d\n",
+                        tmpcomp, tmpprod, tmprev, tmpsize, DEFMAX);
+                *nextdef = c;
+                errors++;
+                continue;
+            }
+
             *nextdef = c;
 
             if (verbose > 1)